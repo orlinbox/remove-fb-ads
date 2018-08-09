23ec716a653dde089156978d7ba120100b113c4d
Orlin Nedyalkov <orlinbox@gmail.com>
2018-08-09 12:10:17 +0300

    Version 2 - remove both Suggested and Sponsored posts.

---
+++ no_fb_ads.js
@@ -0,0 +1,34 @@
+var stringOne = 'Suggested Post';
+var stringTwo = 'SpSonSsoSredS';
+
+function removeUnwantedStuff() {
+  var elArray = [];
+  var spans = document.getElementsByTagName('span');
+  
+  for (var j = 0, l = spans.length; j < l; j++) {
+    if (spans[j].className.indexOf('orlprc') < 0) {
+      elArray.push(spans[j]);
+    }
+  }
+
+  for (var i = 0, n = elArray.length; i < n; i++) {
+    if (typeof elArray[i] !== 'undefined') {
+      if (elArray[i].textContent == stringOne || elArray[i].textContent == stringTwo) {
+        var element = elArray[i];
+        while(element.parentNode) {
+          if (element.id.indexOf('hyperfeed') > -1) {
+            element.remove();
+            console.log("REMOVED: " + elArray[i].textContent);
+            break;
+          }
+          element = element.parentNode;
+        }
+      } else {
+        elArray[i].className += ' orlprc';
+      }
+    }
+  }
+}
+
+removeUnwantedStuff();
+setInterval(removeUnwantedStuff, 3000);
